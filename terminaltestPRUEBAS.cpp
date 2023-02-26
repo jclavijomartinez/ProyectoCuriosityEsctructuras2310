6b049283a78f6345e49533f070de1f309acaf187
Juan Clavijo <jclavijomartinez@gmail.com>
2023-02-25 21:39:47 -0500

    error corregido

--- terminaltestPRUEBAS.cpp
+++ terminaltestPRUEBAS.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <string>
 
-void printPrompt() {
+void imprimirSimbolo() {
   std::cout << "$ ";
 }
 
 int main() {
   std::string input;
   while (true) {
-    printPrompt();
+    imprimirSimbolo();
     std::getline(std::cin, input);
     if (input == "exit") {
       break;
     }
     if (input == "ayuda") {
-      std::cout<<"Bienvenido al controlador! en esta seccion puedes encontrar todas las funciones, proximamente"
+      std::cout<<"Bienvenido al controlador! en esta seccion puedes encontrar todas las funciones, proximamente";
       break;
     }
   }
