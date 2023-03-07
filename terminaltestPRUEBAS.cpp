9c52137ef9fba6c2ab0bd00d812244ca6691c9f2
PabloRodriguezPUJ <pablo.rodriguezg@javeriana.edu.co>
2023-03-06 20:43:47 -0500

    agregar_elemento done

--- terminaltestPRUEBAS.cpp
+++ terminaltestPRUEBAS.cpp
@@ -87,7 +87,38 @@ int main() {
                 cout << "Comentario: " << comentario << endl;
             } else if (input == "agregar_elemento") {
                 cout << "Opción elegida: agregar_elemento" << endl;
-            } else if (input == "ayuda") {
+
+                char tipo_componente;
+                double tamano;
+                char unidad_medida;
+                int coord_x, coord_y;
+
+    sscanf(input.c_str(), "%*c %c %lf %c %d %d", &tipo_componente, &tamano, &unidad_medida, &coord_x, &coord_y);
+    if(tipo_componente == 'r' || tipo_componente == 'c' || tipo_componente == 'm' || tipo_componente == 'd'){
+        if(tamano <= 100){
+            if(unidad_medida == 'c' || unidad_medida == 'm'){
+                if(coord_x >= 0 && coord_x <= 300 && coord_y >= 0 && coord_y <= 300){
+                    // Aquí puedes agregar la lógica para agregar el elemento al sistema
+                    cout << "Elemento agregado exitosamente" << endl;
+                }
+                else{
+                    cout << "Coordenadas invalidas, deben ser entre 0 y 300" << endl;
+                }
+            }
+            else{
+                cout << "Unidad de medida invalida, solo se acepta c para centimetros y m para metros" << endl;
+            }
+        }
+        else{
+            cout << "Tamano invalido, debe ser menor o igual a 100" << endl;
+        }
+    }
+    else{
+        cout << "Tipo de componente invalido, solo se acepta r para Roca, c para Crater, m para Monticulo y d para Duna" << endl;
+    }
+    
+            } 
+            else if (input == "ayuda") {
                 
                 cout << "Comándos válidos :" << endl;
                 cout << "  cargar_comandos nombre_archivo" << endl;
